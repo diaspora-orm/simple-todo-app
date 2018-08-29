8b5d150c28f48d8062028feb9fa8a3005a7ea57f
Gerkin <nihilivin@gmail.com>
2018-08-30 01:27:54 +0200

    Declare ITodo

--- src/dataStore.ts
+++ src/dataStore.ts
@@ -2,3 +2,10 @@ import { Diaspora } from '@diaspora/diaspora';
 
 // Initialize the data source
 Diaspora.createNamedDataSource( 'main', 'inMemory' );
+
+// Interface describing the attributes of a ToDo item.
+// Because the ID is a property of the Entity, we don't declare it here.
+export interface ITodo{
+	label: string;
+	finished: boolean;
+}
