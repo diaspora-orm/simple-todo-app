1cc3a51dfe45cfbbbd2fe098bce04fb77da66783
Gerkin <nihilivin@gmail.com>
2018-08-31 16:46:38 +0200

    Implement searches, displaying all todos

--- src/components/App.vue
+++ src/components/App.vue
@@ -31,14 +31,60 @@
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
+import { Entity, Set } from '@diaspora/diaspora';
 
-import { ToDos } from '../dataStore';
+import { ToDos, ITodo } from '../dataStore';
 import TodoItemComponent from './TodoItem.vue';
 
 @Component( {
 	components: { TodoItemComponent },
 } )
 export default class AppComponent extends Vue {
+	// # ToDos searches
+
+	// ## ToDos currently displayed
+	public displayedTodos: Set<ITodo> | null = null;
+
+	// ## Boolean checking if the data source contains at least one ToDo
+	// It is used to display or hide the header & footer.
+	public hasTodos = false;
+
+	// ## Boolean checking if all ToDos are finished
+	private allTodosFinished = false;
+
+	// ## Unfinished ToDos
+	// Used in the footer's text
+	public leftTodos = 0;
+
+
+	// # Search
+
+	// Refresh all ToDos searches.
+	private async refreshToDoSearches(){
+		let allTodos: Set<ITodo>;
+		let hasTodos: Entity<ITodo> | null;
+		let leftTodos: Set<ITodo>;
+
+		[allTodos, hasTodos, leftTodos] = await Promise.all( [
+			// Assign to `displayedTodos`, and check if all are finished for `allTodosFinished`
+			ToDos.findMany(),
+			// Check if null for `hasTodos`
+			ToDos.find(),
+			// Count the results for `leftTodos`
+			ToDos.findMany( { finished: false } ),
+		] );
+		this.displayedTodos = allTodos;
+
+		// Assign to properties requiring additionnal logic
+		this.allTodosFinished = allTodos
+			.toChainable(Set.ETransformationMode.ATTRIBUTES)
+			.every( todo => todo.finished )
+			.value();
+		this.hasTodos = hasTodos !== null;
+		this.leftTodos = leftTodos.length;
+	}
+
+
 	// # Initialization
 
 	// When starting up the app, insert fake data
@@ -47,7 +93,7 @@ export default class AppComponent extends Vue {
 			{ label: 'Check the documentation', finished: true },
 			{ label: 'Finish the tutorial', finished: false },
 		] );
-		// TODO: Do searches with the model...
+		await this.refreshToDoSearches();
 	}
 }
 </script>
